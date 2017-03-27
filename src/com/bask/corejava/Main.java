4257595090d57ea1055502b5b6014572cde138e0
Baskoro Indrayana <baskoroindrayana@protonmail.com>
2017-03-27 19:23:22 +0700

    Solved Exercise 13

--- src/com/bask/corejava/Main.java
+++ src/com/bask/corejava/Main.java
@@ -6,7 +6,8 @@
 package com.bask.corejava;
 
 //import com.bask.corejava.ch2.CalendarPrinter;
-import com.bask.corejava.ch2.Car;
+import com.opencsv.CSVReader;
+import java.io.FileReader;
 
 /**
  * Main class for other classes in the exercise.
@@ -14,21 +15,33 @@ import com.bask.corejava.ch2.Car;
  */
 public class Main {
     
-    public static void main(String[] args) {
-        Car car = new Car(23.6);
-        System.out.println("Fuel #1: " + car.getFuel());
-        System.out.println("Mileage #1: " + car.getMileage());
+    public static void main(String[] args) throws Exception {
         
-        car.travel(200);
-        System.out.println("Fuel #2: " + car.getFuel());
-        System.out.println("Mileage #2: " + car.getMileage());
+        CSVReader reader = new CSVReader(
+                new FileReader("SacramentocrimeJanuary2006.csv"));
         
-        car.refuel(10);
-        System.out.println("Fuel #3: " + car.getFuel());
-        System.out.println("Mileage #3: " + car.getMileage());
+        String[] nextLine;
         
-        car.travel(300);
-        System.out.println("Fuel #4: " + car.getFuel());
-        System.out.println("Mileage #4: " + car.getMileage());
+        while ((nextLine = reader.readNext()) != null) {
+            for (int i = 0; i < nextLine.length; i++) 
+                System.out.printf("| %4s ", nextLine[i]);
+            System.out.println("|");
+        }
+        
+//        Car car = new Car(23.6);
+//        System.out.println("Fuel #1: " + car.getFuel());
+//        System.out.println("Mileage #1: " + car.getMileage());
+//        
+//        car.travel(200);
+//        System.out.println("Fuel #2: " + car.getFuel());
+//        System.out.println("Mileage #2: " + car.getMileage());
+//        
+//        car.refuel(10);
+//        System.out.println("Fuel #3: " + car.getFuel());
+//        System.out.println("Mileage #3: " + car.getMileage());
+//        
+//        car.travel(300);
+//        System.out.println("Fuel #4: " + car.getFuel());
+//        System.out.println("Mileage #4: " + car.getMileage());
     }
 }
